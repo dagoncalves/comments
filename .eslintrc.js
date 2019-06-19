baf3617b5564f62f93653436750a3565d3972715
Deivys <deivysgoncalves@gmail.com>
2019-06-19 15:00:02 -0300

    Update .eslintrc.js
    Corrigindo o eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,72 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    jest: true
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      jsx: true
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest": true
     },
-    sourceType: "module"
-  },
-  plugins: ["react"],
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
-    "space-in-parens": ["error", "always"],
-    "array-bracket-spacing": ["error", "always"],
-    "space-before-function-paren": ["error", "always"],
-    "space-before-blocks": ["error", "always"],
-    "space-infix-ops": [2],
-    "space-unary-ops": [
-      1,
-      {
-        words: true,
-        nonwords: true
-      }
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
     ],
-    "spaced-comment": [2, "always"],
-    "arrow-spacing": [2]
-  }
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "space-in-parens": [
+            "error",
+            "always"
+        ],
+        "array-bracket-spacing": [
+            "error",
+            "always"
+        ],
+        "space-before-function-paren": [
+            "error",
+            "always"
+        ],
+        "space-before-blocks": [
+            "error",
+            "always"
+        ],
+        "space-infix-ops": [
+            2
+        ],
+        "space-unary-ops": [
+            1,
+            {
+                "words": true,
+                "nonwords": true
+            }
+        ],
+        "spaced-comment": [
+            2,
+            "always"
+        ],
+        "arrow-spacing": [
+            2
+        ]
+    }
 };
